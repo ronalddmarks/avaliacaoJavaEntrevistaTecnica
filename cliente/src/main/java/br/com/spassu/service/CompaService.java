d1c717684c509eaeae237ddd4d7f28e1dc300f8c
Ronald <ronald.dmarks@gmail.com>
2021-01-10 18:12:25 -0300

    tese formatar data

--- cliente/src/main/java/br/com/spassu/service/CompaService.java
+++ cliente/src/main/java/br/com/spassu/service/CompaService.java
@@ -2,6 +2,7 @@ package br.com.spassu.service;
 
 import java.sql.Date;
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import br.com.spassu.modelo.Cliente;
@@ -9,7 +10,7 @@ import br.com.spassu.modelo.Compra;
 
 public class CompaService {
 
-   public void incluirCompra(String valor) {
+   public void incluirCompra(String valor) throws ParseException {
 	   
 		String[] textoSeparado = valor.split(";");
 		
@@ -26,7 +27,7 @@ public class CompaService {
 	   compra.setValor(NumberFormat.getCurrencyInstance().format(textoSeparado[3]));
 	   
 	   
-	   compra.setCliente(cliente.getId(Integer.parseInt(textoSeparado[1])));
+	//   compra.setCliente(cliente.getId(Integer.parseInt(textoSeparado[1])));
 	   
 	   
 	   
